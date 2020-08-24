9bfcf75caeb7abb9387511ef45f26b877d56fa34
unknown <sasocano1@gmail.com>
2020-08-24 18:34:18 +0200

    Updates

---
+++ precache-manifest.fffeff2a58cb413f99a42a8712cc2026.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e53a74c1038cca833846cc977373109f",
+    "url": "/Project11/index.html"
+  },
+  {
+    "revision": "68ac024a784b23ff89c7",
+    "url": "/Project11/static/css/2.ed439b8d.chunk.css"
+  },
+  {
+    "revision": "65fd5ecf5d322a0593c5",
+    "url": "/Project11/static/css/main.c66cc1b6.chunk.css"
+  },
+  {
+    "revision": "68ac024a784b23ff89c7",
+    "url": "/Project11/static/js/2.69157d9c.chunk.js"
+  },
+  {
+    "revision": "2a4fe06955d291b5aaa01a34195c0f63",
+    "url": "/Project11/static/js/2.69157d9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65fd5ecf5d322a0593c5",
+    "url": "/Project11/static/js/main.5005f17b.chunk.js"
+  },
+  {
+    "revision": "3d28677a1acdbfae927e",
+    "url": "/Project11/static/js/runtime-main.9c1431fb.js"
+  },
+  {
+    "revision": "d0842fa95aa8dbb3f350844841ae8e73",
+    "url": "/Project11/static/media/brainster.d0842fa9.png"
+  },
+  {
+    "revision": "4e882a6562bd4853bd44eb940e5d3423",
+    "url": "/Project11/static/media/data-science.4e882a65.jpg"
+  },
+  {
+    "revision": "15ddfcb279384ef76907d3c301ca1f59",
+    "url": "/Project11/static/media/design.15ddfcb2.jpg"
+  },
+  {
+    "revision": "241f0e3f839e4487208c2ccf6e4c2772",
+    "url": "/Project11/static/media/marketing.241f0e3f.jpg"
+  },
+  {
+    "revision": "52b571c6ab19b174821cc8af5eedda34",
+    "url": "/Project11/static/media/uxui.52b571c6.jpg"
+  }
+]);
